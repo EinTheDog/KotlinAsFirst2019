29df20818f9a452ed1664bc712eda806353bdeac
Maxim Alpatski <alpatskij.mi@edu.spbstu.ru>
2019-09-18 16:55:07 +0300

    Problems 1-2 in Lesson4 are solved

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -130,6 +130,7 @@ fun abs(v: List<Double>): Double {
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
 fun mean(list: List<Double>): Double {
+    //c
     var ans = 0.0
     for (i in list.indices) ans += list[i]
     return if (list.isNotEmpty()) ans / list.size else 0.0
