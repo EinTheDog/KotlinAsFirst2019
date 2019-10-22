4ffeb15f1a5eaa9569d303bf837a54de6c7eba42
Maxim Alpatski <alpatskij.mi@edu.spbstu.ru>
2019-10-22 19:38:36 +0300

    Lesson6 rework (6)

--- test/lesson6/task1/Tests.kt
+++ test/lesson6/task1/Tests.kt
@@ -166,6 +166,6 @@ class Tests {
         assertThrows(IllegalArgumentException::class.java) { computeDeviceCells(10, "+>+>[+>", 3) }
         assertThrows(IllegalStateException::class.java) { computeDeviceCells(20, ">>>>>>>>>>>>>", 12) }
         assertThrows(IllegalStateException::class.java) { computeDeviceCells(2, "<<", 500) }
-        assertThrows(IllegalArgumentException::class.java) { computeDeviceCells(1, "--+<+>+<][++", 97378) }
+        assertThrows(IllegalStateException::class.java) { computeDeviceCells(1, "--+<+>+<][++", 97378) }
     }
 }
